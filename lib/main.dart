b79651d08b672b3d0cc3c24b7e85f6a2edbe3c98
VANOT Alexy <alexystefen@hotmail.com>
2024-10-11 19:20:46 +0200

    V2 Restylization, suppression par glissement, etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:shared_preferences/shared_preferences.dart'; // save feature
-import 'dart:convert'; // pour json
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +28,7 @@ class TodoListScreen extends StatefulWidget {
 class _TodoListScreenState extends State<TodoListScreen> {
   final List<TodoItem> _todos = [];
   final TextEditingController _controller = TextEditingController();
-  String _filter = 'all'; // filtrage des todo
+  String _filter = 'all';
 
   @override
   void initState() {
@@ -74,9 +74,38 @@ class _TodoListScreenState extends State<TodoListScreen> {
     });
   }
 
+  Future<bool?> _confirmDelete(TodoItem todo) async {
+    bool? confirm = await showCupertinoDialog<bool>(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('Confirmation de Suppression'),
+          content: Text('Êtes-vous sûr de vouloir supprimer "${todo.title}" ?'),
+          actions: [
+            CupertinoDialogAction(
+              child: const Text('Annuler'),
+              onPressed: () => Navigator.of(context).pop(false),
+            ),
+            CupertinoDialogAction(
+              child: const Text(
+                'Supprimer',
+                style: TextStyle(
+                  color: CupertinoColors.destructiveRed,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () => Navigator.of(context).pop(true),
+            ),
+          ],
+        );
+      },
+    );
+
+    return confirm;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Filtrer les tâches
     List<TodoItem> filteredTodos = _filter == 'completed'
         ? _todos.where((todo) => todo.isDone).toList()
         : _filter == 'incomplete'
@@ -87,97 +116,131 @@ class _TodoListScreenState extends State<TodoListScreen> {
       navigationBar: const CupertinoNavigationBar(
         middle: Text('Flutter Todo List'),
       ),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 60.0, horizontal: 20.0),
-            child: CupertinoTextField(
-              controller: _controller,
-              placeholder: 'Ex: Ne pas oublier de...',
-            ),
-          ),
-          CupertinoButton.filled(
-            onPressed: _addTodo,
-            child: const Text('Ajouter'),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              _buildFilterButton('all', 'Tous'),
-              _buildFilterButton('completed', 'Complètes'),
-              _buildFilterButton('incomplete', 'Incomplètes'),
-            ],
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: filteredTodos.length,
-              itemBuilder: (context, index) {
-                return Dismissible(
-                  key: Key(filteredTodos[index].title),
-                  background: Container(color: CupertinoColors.destructiveRed),
-                  onDismissed: (direction) {
-                    setState(() {
-                      _todos.remove(filteredTodos[index]);
-                      _saveTodos();
-                    });
-                    // Cupertino Dialog pour la confirmation de suppression
-                    CupertinoAlertDialog alert = CupertinoAlertDialog(
-                      title: const Text('Todo supprimée'),
-                      content: Text("${filteredTodos[index].title} a été supprimée."),
-                      actions: [
-                        CupertinoDialogAction(
-                          child: const Text('OK'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    );
-                    showCupertinoDialog(
-                      context: context,
-                      builder: (context) => alert,
-                    );
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                filteredTodos[index].isDone = !filteredTodos[index].isDone;
-                                _saveTodos();
-                              });
-                            },
-                            child: Text(
-                              filteredTodos[index].title,
-                              style: TextStyle(
-                                decoration: filteredTodos[index].isDone
-                                    ? TextDecoration.lineThrough
-                                    : null,
-                              ),
-                            ),
+              const SizedBox(height: 20),
+              Container(
+                constraints: const BoxConstraints(maxWidth: 600),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 50.0, bottom: 15.0),
+                  child: CupertinoTextField(
+                    controller: _controller,
+                    placeholder: 'Ex: Ne pas oublier de...',
+                  ),
+                ),
+              ),
+              CupertinoButton.filled(
+                onPressed: _addTodo,
+                child: const Text(
+                  'Ajouter',
+                  style: TextStyle(
+                    color: CupertinoColors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                constraints: const BoxConstraints(maxWidth: 600),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _buildFilterButton('all', 'Tous'),
+                    _buildFilterButton('completed', 'Complètes'),
+                    _buildFilterButton('incomplete', 'Incomplètes'),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                child: Container(
+                  constraints: const BoxConstraints(maxWidth: 600),
+                  child: ListView.builder(
+                    itemCount: filteredTodos.length,
+                    itemBuilder: (context, index) {
+                      return Dismissible(
+                        key: Key(filteredTodos[index].title),
+                        background: Container(
+                          color: CupertinoColors.destructiveRed,
+                          alignment: Alignment.centerLeft,
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: const Text(
+                            'Supprimer',
+                            style: TextStyle(color: CupertinoColors.white),
                           ),
                         ),
-                        CupertinoSwitch(
-                          value: filteredTodos[index].isDone,
-                          onChanged: (value) {
+                        secondaryBackground: Container(
+                          color: CupertinoColors.destructiveRed,
+                          alignment: Alignment.centerRight,
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: const Text(
+                            'Supprimer',
+                            style: TextStyle(color: CupertinoColors.white),
+                          ),
+                        ),
+                        direction: DismissDirection.endToStart,
+                        confirmDismiss: (direction) async {
+                          if (direction == DismissDirection.endToStart) {
+                            return await _confirmDelete(filteredTodos[index]);
+                          }
+                          return false;
+                        },
+                        onDismissed: (direction) {
+                          if (direction == DismissDirection.endToStart) {
                             setState(() {
-                              filteredTodos[index].isDone = value;
+                              _todos.remove(filteredTodos[index]);
                               _saveTodos();
                             });
-                          },
+                          }
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      filteredTodos[index].isDone = !filteredTodos[index].isDone;
+                                      _saveTodos();
+                                    });
+                                  },
+                                  child: Text(
+                                    filteredTodos[index].title,
+                                    style: TextStyle(
+                                      decoration: filteredTodos[index].isDone
+                                          ? TextDecoration.lineThrough
+                                          : null,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              CupertinoSwitch(
+                                value: filteredTodos[index].isDone,
+                                onChanged: (value) {
+                                  setState(() {
+                                    filteredTodos[index].isDone = value;
+                                    _saveTodos();
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
                         ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
@@ -188,13 +251,14 @@ class _TodoListScreenState extends State<TodoListScreen> {
         ? Container(
             decoration: BoxDecoration(
               color: CupertinoColors.activeBlue,
-              borderRadius: BorderRadius.circular(12),
+              borderRadius: BorderRadius.circular(10),
             ),
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
             child: Text(
               label,
               style: const TextStyle(
                 color: CupertinoColors.white,
+                fontSize: 16.0,
               ),
             ),
           )
@@ -203,7 +267,8 @@ class _TodoListScreenState extends State<TodoListScreen> {
             child: Text(
               label,
               style: const TextStyle(
-                color: CupertinoColors.activeBlue,
+                color: CupertinoColors.inactiveGray,
+                fontSize: 16.0,
               ),
             ),
           );
